b63ab5dcdf9c9722b88c01e71cbbc3d8fa6c71ad
Juan Montemayor <jam1@alum.mit.edu>
2015-09-14 16:56:47 -0500

    Adding ECScraper Stub
    Adding ecscraper.py this is where the initial scraper code will live.

---
+++ tests/test_ecscraper.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+import threecorner.ecscraper as ec_scraper
+
+
+def test_basic():
+    """Control Test, if this fails we have bigger problems"""
+    print("I RAN!")
+
+
+def test_boo():
+    """Make sure boo work"""
+    ec_scraper.boo()
