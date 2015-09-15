17a05b0a9c747e2604bbf9a4a5209f644747b0df
Juan Montemayor <jam1@alum.mit.edu>
2015-09-15 18:02:52 -0500

    WIP: Login and Hit RC Resource Site
    Still need to drill into the links, probably going to just hard code
    them in.

--- threecorner/ecscraper.py
+++ threecorner/ecscraper.py
@@ -1,11 +1,13 @@
 # -*- coding: utf-8 -*-
 from scrapy.crawler import CrawlerProcess
+from scrapy.selector import HtmlXPathSelector
+from scrapy.http import Request
 import scrapy
 
 
 class LoginSpider(scrapy.Spider):
     name = 'mitec'
-    start_urls = ['https://alumsso.mit.edu/cas/login?service=https%3A%2F%2Falum.mit.edu%2Faccount%2Fj_spring_cas_security_check']
+    start_urls = ['https://alumsso.mit.edu/cas/login']
 
     def parse(self, response):
         return scrapy.FormRequest.from_response(
@@ -16,15 +18,25 @@ class LoginSpider(scrapy.Spider):
 
     def after_login(self, response):
         # check login succeed before going on
-        if "authentication failed" in response.body:
+        if "The username and/or password you provided are incorrect" in response.body:
             self.logger.error("Login failed")
             return
 
         # continue scraping with authenticated session...
         # https://stargate.mit.edu/ectrackweb/member/resources/rcvc/stats.mit
-        def make_requests_from_url(self, url):
-            url = "https://stargate.mit.edu/ectrackweb/member/resources/rcvc/stats.mit"
 
+        else:
+            return Request(url="https://stargate.mit.edu/ectrackweb/member/resources/rcvc/rcvc.mit",
+                           callback=self.parse_page)
+
+    def parse_page(self, response):
+        hxs = HtmlXPathSelector(response)
+        links = hxs.select('//@href')
+
+        for link in links:
+            # yield Request(url=link, callback=self.parse_page)
+            # yield {'url': link}
+            self.logger.info(link)
 
 def boo():
     process = CrawlerProcess({'USER_AGENT': 'Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 5.1)'})
